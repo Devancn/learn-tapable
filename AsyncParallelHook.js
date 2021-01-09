a68ce2db3db8b23c56e5b2ccf31fe4da2b64c80a
dingxiaohuan <wydingxiaohuan@163.com>
2021-01-09 18:24:00 +0800

    AsyncParallelHook example

---
+++ AsyncParallelHook.js
@@ -0,0 +1,19 @@
+let { AsyncParallelHook } = require("tapable");
+/*
+let hook = new AsyncParallelHook();
+hook.tap("1", (name) => {
+  console.log(1, name);
+});
+
+hook.tap("2", (name) => {
+  console.log(2, name);
+});
+
+hook.tap("3", (name) => {
+  console.log(3, name);
+});
+
+hook.callAsync("Devan", (err) => {
+  console.log(err);
+});
+*/
