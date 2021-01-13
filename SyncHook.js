7dbdc3be66c7f78fdfd85422a3d1e72b38f8cf26
dingxiaohuan <wydingxiaohuan@163.com>
2021-01-13 20:53:25 +0800

    hook call stage

--- SyncHook.js
+++ SyncHook.js
@@ -3,7 +3,7 @@ let { SyncHook } = require("tapable");
 /**
  * 同步钩子
  * 方法的参数为数组，会作为执行hook时的参数列表的个数
- */
+
 // 创建钩子
 const hook = new SyncHook(["name", "age"]);
 
@@ -24,3 +24,24 @@ hook.tap("3", (name, age) => {
 // 执行钩子
 
 hook.call('Devan', 27)
+ */
+
+ let hook = new SyncHook(['name']);
+
+ hook.tap({name: 'tap1', stage: 1}, name => {
+   console.log('tap1', name)
+ })
+
+ hook.tap({name: 'tap3', stage:3}, name => {
+  console.log('tap3', name)
+})
+
+hook.tap({name: 'tap4', stage: 4}, name => {
+  console.log('tap4', name)
+})
+
+hook.tap({name: 'tap2', stage: 2 }, name => {
+  console.log('tap2', name)
+})
+
+hook.call('Devan')
